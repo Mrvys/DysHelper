e2663f453298cd64aaf6d85a3fb074cf169501b1
Mrvys <mrvy13@gmail.com>
2016-07-05 23:57:57 +0200

    Update background.js

--- DysHelper/background.js
+++ DysHelper/background.js
@@ -32,52 +32,52 @@ function divider(){
                                 alphabet = alphabet + data[counter];
                                 counter++;
                             }
-			    alphabet = alphabet.replace(/,/g, "");
- -                            alphabet = $.trim(alphabet);
- -                            var sumOfCharsToDivide = 0;
- -                            var i = counter + 1;
- -                            var counterOfLines = 0;
- -							//parsing matrix to find chars that need to be split
- -                            while (i < data.length - 1) {
- -                                var line = [];
- -                                while (data[i] != '\n' && data.length > i) {
- -                                    line = line + data[i];
- -                                    i++;
- -                                }
- -                                var j = 0;
- -                                var counterOfWord = 0;
- -                                while (j < line.length - 1) {
- -                                    var word = [];
- -                                    while (line[j] != "," && line[j] != '\n' && j < line.length) {
- -                                        word = word + line[j];
- -                                        j++;
- -                                    }
- -                                    if (word > border) {
- -                                        var newCharacter = alphabet[counterOfLines] + alphabet[counterOfWord];
- -                                        charsToStore = charsToStore + " " + newCharacter;
- -                                        sumOfCharsToDivide++;
- -                                    }
- -                                    counterOfWord++;
- -                                    j++;
- -                                }
- -                                counterOfLines++;
- -                                i++;
- -                            }
- -                            var trimData = $.trim(charsToStore);
- -                            expressionToFind = trimData.replace(/ /g, '|');
- -                            saveString();
+                            alphabet = alphabet.replace(/,/g, "");
+                            alphabet = $.trim(alphabet);
+                            var sumOfCharsToDivide = 0;
+                            var i = counter + 1;
+                            var counterOfLines = 0;
+		            //parsing matrix to find chars that need to be split
+                            while (i < data.length - 1) {
+                                var line = [];
+                                while (data[i] != '\n' && data.length > i) {
+                                    line = line + data[i];
+                                    i++;
+                                }
+                                var j = 0;
+                                var counterOfWord = 0;
+                                while (j < line.length - 1) {
+                                    var word = [];
+                                    while (line[j] != "," && line[j] != '\n' && j < line.length) {
+                                        word = word + line[j];
+                                        j++;
+                                    }
+                                    if (word > border) {
+                                        var newCharacter = alphabet[counterOfLines] + alphabet[counterOfWord];
+                                        charsToStore = charsToStore + " " + newCharacter;
+                                        sumOfCharsToDivide++;
+                                    }
+                                    counterOfWord++;
+                                    j++;
+                                }
+                                counterOfLines++;
+                                i++;
+                            }
+                            var trimData = $.trim(charsToStore);
+                            expressionToFind = trimData.replace(/ /g, '|');
+                            saveString();
                         }
                     });
                 }
             }
         });
 		
-		//get user id from storage
-		chrome.storage.sync.get('userID', function (result) {
-			userId = result.userID;
-		});
+	//get user id from storage
+	chrome.storage.sync.get('userID', function (result) {
+		userId = result.userID;
+	});
 		
-		//get pair of characters from chrome storage
+	//get pair of characters from chrome storage
         chrome.storage.sync.get('data', function (result) {
             expressionToFind = result.data;
             if (expressionToFind) {
